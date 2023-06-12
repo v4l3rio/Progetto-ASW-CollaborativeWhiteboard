51766b6fa532c4ac05bcb969ed729f456a720676
FreshMag <pescomagnani@gmail.com>
2023-06-12 11:18:50 +0200

    Login works: missing cookies management

--- backend/auth/test/auth_test.js
+++ backend/auth/test/auth_test.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const {Authenticator} = require("../Authenticator");
 const app = express();
+const {serialize} = require("cookie");
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(express.json())
 const port = 3000;
 
@@ -11,7 +14,6 @@ class Db {
     }
 
     async findOne(username) {
-        //console.log(this.users)
         for (let i = 0; i < this.users.length; i++) {
             if (this.users[i].username === username) {
                 return this.users[i];
@@ -59,6 +61,51 @@ app.post('/register', (req, res) => {
                 "first name and last name"})
     }
 })
+
+app.post("/login", (req, res) => {
+    if (req.body.username && req.body.password) {
+        const {username, password} = req.body;
+        auth.login({username:username, password:password})
+            .then((result) => {
+                if (result.err) {
+                    console.log("Error: " + result.err)
+                    res.status(400).json({message: result.err})
+                } else {
+                    const logged = result.user;
+                    const noPasswordUser = {id: logged.id, username:logged.username};
+                    /*const serialized = serialize('token', logged.token, {
+                        httpOnly: true,
+                        secure: process.env.NODE_ENV === 'production',
+                        sameSite: 'strict',
+                        maxAge: 60 * 60 * 24 * 30,
+                        path: '/',
+                    });
+
+                     */
+                    res.cookie("token", logged.token, {httpOnly: true, secure: true, maxAge:60 * 60 * 24 * 30})
+
+                    console.log("Logged user " + JSON.stringify(noPasswordUser));
+                    res.status(200)
+                        .json({"message": "User logged successfully",
+                            "user" : noPasswordUser});
+                }
+
+            });
+    } else if (req.cookies && req.cookies.token){
+        auth.validateToken(req.cookies.token).then(result => {
+            if (result.err) {
+                res.status(401).json({"message": "Invalid Token"});
+            } else {
+                res.status(200)
+                    .json({"message": "User logged successfully", "decoded": result.token});
+            }
+        })
+    } else {
+        const {cookies} = req;
+        console.log(cookies)
+        res.status(400).json({"message" : "Bad input, please provide username and password"})
+    }
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
