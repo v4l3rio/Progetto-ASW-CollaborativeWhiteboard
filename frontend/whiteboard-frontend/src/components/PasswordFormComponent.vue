0e62390f256f4c5918115b2dd8ed1a01895f46e4
ThomasCapelli <thomas.capelli@studio.unibo.it>
2023-06-15 15:33:02 +0200

    Implemented Login form with his components and implemented the control logic

---
+++ frontend/whiteboard-frontend/src/components/PasswordFormComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="input-group mb-3">
+            <div class="form-floating">
+                <input :type="isVisible ? 'text' : 'password'" class="form-control" id="floatingPassword" placeholder="Username" aria-required="true" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="btn btn-outline-secondary" type="button" id="button-addon1" @click="showPassword">
+                <img :src="require('../assets/icons/' + image)" alt="show password"/>
+            </button>
+        </div>
+</template>
+<script>
+export default {
+    name: "PasswordFormComponent",
+    data(){
+        return{
+            image: 'eye-slash.svg',
+            iconHide: 'eye-slash.svg',
+            iconShow: 'eye.svg', 
+            isVisible: false,
+        }
+    },   
+    methods: {
+        init() {
+            if (this.isVisible) {
+                this.image = this.iconShow;
+            } else {
+                this.image = this.iconHide;
+            }
+        },
+        showPassword() {
+            this.isVisible = this.isVisible ? false : true;
+            this.init();
+        }
+    }
+}
+</script>
--- frontend/whiteboard-frontend/src/views/LoginView.vue
+++ frontend/whiteboard-frontend/src/views/LoginView.vue
@@ -1,62 +1,42 @@
 <template>
-    <p>Questa pagina di login non funziona, è stata utilizzata solo come esempio per il routing</p>
     <div class="d-flex justify-content-center">
-    <div class="col-8">
+    <div class="col-4 ">
     <form>
-        <!-- Email input -->
-        <div class="form-outline mb-4">
-            <input type="email" id="form2Example1" class="form-control" />
-            <label class="form-label" for="form2Example1">Email address</label>
-        </div>
-
-        <!-- Password input -->
-        <div class="form-outline mb-4">
-            <input type="password" id="form2Example2" class="form-control" />
-            <label class="form-label" for="form2Example2">Password</label>
-        </div>
-
-        <!-- 2 column grid layout for inline styling -->
-        <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-                <!-- Checkbox -->
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                    <label class="form-check-label" for="form2Example31"> Remember me </label>
-                </div>
-            </div>
-
-            <div class="col">
-                <!-- Simple link -->
-                <a href="#!">Forgot password?</a>
-            </div>
-        </div>
-
+        <h1 class="h3 mb-2">Login</h1>
+        <EmailFormComponent></EmailFormComponent>
+        <PasswordFormComponent></PasswordFormComponent>
         <!-- Submit button -->
-        <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+        <div class="form-check text-start my-3">
+            <input class="form-check-input" type="checkbox" value="remember me" id="remeberMeCheckBox">
+            <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+        </div>
+        <div class="form-floating mb-3">
+            <button type="submit" class="btn btn-primary col-12" id="SignInButton" >Sign in</button>
+        </div>
 
         <!-- Register buttons -->
         <div class="text-center">
             <p>Not a member? <a href="#!">Register</a></p>
-            <p>or sign up with:</p>
-            <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-facebook-f"></i>
-            </button>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-google"></i>
-            </button>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-twitter"></i>
-            </button>
-
-            <button type="button" class="btn btn-link btn-floating mx-1">
-                <i class="fab fa-github"></i>
-            </button>
         </div>
     </form>
     </div>
     </div>
 </template>
-<script setup>
+<script>
+import EmailFormComponent from "@/components/EmailFormComponent.vue"
+import PasswordFormComponent from "@/components/PasswordFormComponent.vue";
+export default {
+    name: 'LoginView',
+    components: {
+        EmailFormComponent,
+        PasswordFormComponent
+    },
+    methods: {
+        enableButton() {
+
+        }
+    },
+    mounted: function () {
+    }
+}
 </script>
