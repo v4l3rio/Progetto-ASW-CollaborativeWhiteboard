e0d5151adb9d50e4a4e4726cca2cc20cf63313f0
FreshMag <pescomagnani@gmail.com>
2023-06-21 10:52:15 +0200

    Added RegisterView.vue

--- frontend/whiteboard-frontend/src/App.vue
+++ frontend/whiteboard-frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Whiteboard</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/register">Register</router-link>
   </nav>
   <router-view/>
 </template>
