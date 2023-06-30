9d95daafbc958a714ef26451d644b565f8da893a
FreshMag <pescomagnani@gmail.com>
2023-06-30 11:34:20 +0200

    The submit button was working...Didn't notice that. Still has some bugs.

--- frontend/whiteboard-frontend/src/views/RegisterView.vue
+++ frontend/whiteboard-frontend/src/views/RegisterView.vue
@@ -2,53 +2,57 @@
     <p>Questa pagina di register non funziona, è stata utilizzata solo come esempio per il routing</p>
     <div class="d-flex justify-content-center">
     <div class="col-8">
-    <form @submit.prevent="submitForm">
-
         <div v-if="showAlert" class="alert alert-danger" role="alert">
             {{alertMessage}}
         </div>
-        <!-- Email input -->
-        <div class="form-outline mb-4">
-            <input type="text" id="username" class="form-control" v-model="username" required/>
-            <label class="form-label" for="username">Username</label>
+        <div v-if="isFormSubmitted" class="alert alert-info" role="alert">
+            <p>Registered successfully</p>
+            <a href="#/login">Go to Login</a>
         </div>
+        <form @submit.prevent="submitForm" v-if="!isFormSubmitted">
 
-        <!-- Name input -->
-        <div class="form-outline mb-4">
-            <input type="text" id="name" class="form-control" v-model="name" required/>
-            <label class="form-label" for="email">Name</label>
-        </div>
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <input type="text" id="username" class="form-control" v-model="username" required/>
+                <label class="form-label" for="username">Username</label>
+            </div>
 
-        <!-- Surname input -->
-        <div class="form-outline mb-4">
-            <input type="text" id="surname" class="form-control" v-model="surname" required/>
-            <label class="form-label" for="surname">Surname</label>
-        </div>
+            <!-- Name input -->
+            <div class="form-outline mb-4">
+                <input type="text" id="name" class="form-control" v-model="name" required/>
+                <label class="form-label" for="name">Name</label>
+            </div>
 
-        <!-- Password input -->
-        <div class="form-outline mb-4">
-            <input type="password" id="password" class="form-control" @input="this.checkPasswords"
-                   v-model="password" required/>
-            <label class="form-label" for="password">Password</label>
-        </div>
+            <!-- Surname input -->
+            <div class="form-outline mb-4">
+                <input type="text" id="lastName" class="form-control" v-model="lastName" required/>
+                <label class="form-label" for="lastName">Surname</label>
+            </div>
 
-        <!-- Confirm input -->
-        <div class="form-outline mb-4">
-            <input type="password" id="confirmPassword" class="form-control" @input="this.checkPasswords"
-                   v-model="confirmPassword" required />
-            <label class="form-label" for="confirmPassword">Confirm Password</label>
-        </div>
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="password" class="form-control" @input="this.checkPasswords"
+                       v-model="password" required/>
+                <label class="form-label" for="password">Password</label>
+            </div>
 
-        <!-- Submit button -->
-        <button class="btn btn-primary btn-block mb-4" :disabled="showAlert">Register</button>
+            <!-- Confirm input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="confirmPassword" class="form-control" @input="this.checkPasswords"
+                       v-model="confirmPassword" required />
+                <label class="form-label" for="confirmPassword">Confirm Password</label>
+            </div>
 
-        <!-- Login buttons -->
-        <div class="text-center">
-            <p>Already a member? <a href="#/login">Login</a></p>
-        </div>
-    </form>
+            <!-- Submit button -->
+            <button class="btn btn-primary btn-block mb-4" :disabled="showAlert" >Register</button>
+
+            <!-- Login buttons -->
+            <div class="text-center">
+                <p>Already a member? <a href="#/login">Login</a></p>
+            </div>
+        </form>
     </div>
-    </div>
+</div>
 </template>
 <script>
 
@@ -60,7 +64,7 @@ export default {
         return {
             username:"",
             name:"",
-            surname:"",
+            lastName:"",
             password:"",
             confirmPassword:"",
             showAlert: false,
@@ -78,28 +82,23 @@ export default {
             }
         },
         submitForm: function () {
-            console.log("ciao")
+            const ref = this;
             axios.post('http://localhost:4000/auth/register', {
                 username:this.username,
                 password:this.password,
-                name:this.name,
-                surname:this.surname
+                first_name:this.name,
+                last_name:this.lastName
             }).then(function (response) {
                 console.log(response);
-                this.isFormSubmitted = true;
+                ref.isFormSubmitted = true;
+                ref.showAlert = false;
             })
             .catch(function (error) {
-                console.log(error);
+                ref.showAlert = true;
+                ref.alertMessage = error;
             });
 
         }
-    },
-    mounted() {
-        //this.username = "cia"
-        //this.name = "ciao"
-        //this.surname = "ciao"
-        //this.password = "ciao"
-        //this.confirmPassword = "ciao"
     }
 }
 </script>
