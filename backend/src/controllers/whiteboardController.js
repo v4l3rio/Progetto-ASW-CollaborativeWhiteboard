28cd590aaa21a0bf95705df7d94f5b462b20953f
FreshMag <pescomagnani@gmail.com>
2023-07-03 22:49:45 +0200

    Started whiteboardController.js and Authorizer.js

---
+++ backend/src/controllers/whiteboardController.js
@@ -0,0 +1,60 @@
+const {Authorizer} = require("../auth/Authorizer");
+const {TestModel} = require("../models/testModel");
+
+const authZ = new Authorizer(TestModel);
+
+/* TODO if everyone can operate freely on the whiteboard, then all the authorize methods
+    can be collapsed in one
+ */
+
+exports.joinWhiteboard = (accessToken, whiteboardId, callback) => {
+    authZ.authorizeToWhiteboard(accessToken, whiteboardId).then(result => {
+        const {err} = result;
+        if (err) {
+            callback(null, err);
+        } else {
+            const whiteboardData = {} // get from DB
+            callback(whiteboardData, undefined);
+        }
+    })
+}
+
+exports.lineStarted = (line, accessToken, whiteboardId, callback) => {
+    authZ.authorizeNewLine(accessToken, whiteboardId).then(result => {
+        const {lineId, err} = result;   // the authorizer generates fresh new line id
+        if (err) {
+            callback(undefined, err);
+        } else {
+            const {point, color} = line;
+            // todo broadcast the line (done by the realtime)
+            callback(lineId, undefined);
+        }
+    })
+}
+
+// maybe unnecessary
+exports.lineMove = (line, lineId, whiteboardId, callback) => {
+    const {point, color} = line;
+    // todo add maybe the authorization also in line move?
+    callback();
+}
+
+exports.lineEnd = (line, accessToken, lineId, whiteboardId, callback) => {
+    authZ.authorizeLineEnd(accessToken, lineId, whiteboardId).then(result => {
+        if (result.err) {
+            callback(result.err)
+        } else {
+            callback()
+        }
+    });
+}
+
+exports.lineDelete = (lineId, accessToken, whiteboardId, callback) => {
+    authZ.authorizeLineDelete(accessToken, lineId, whiteboardId).then(result => {
+        if (result.err) {
+            callback(result.err);
+        } else {
+            callback();
+        }
+    });
+}
