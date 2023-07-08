8eacec7ec08ac58f367a71d837c948fee1325a04
ThomasCapelli <thomas.capelli@studio.unibo.it>
2023-07-08 13:16:08 +0200

    navbar fixes

--- frontend/whiteboard-frontend/src/App.vue
+++ frontend/whiteboard-frontend/src/App.vue
@@ -1,16 +1,6 @@
 <template>
-  <nav>
-      <div class="row">
-          <div class="col"></div>
-          <router-link to="/" class="col">Home</router-link>
-          <!--<router-link to="/whiteboard/" class="col">Whiteboard</router-link>-->
-          <router-link to="/login" class="col">Login</router-link>
-          <router-link to="/addWhiteboard" class="col">Files</router-link>
-          <router-link to="/register" class="col">Register</router-link>
-          <router-link to="/profile" class="col">Profile</router-link>
-         <NotificationComponent class="col"></NotificationComponent>
-      </div>
-  </nav>
+  <NavbarComponent></NavbarComponent>
+  <NotificationComponent class="col"></NotificationComponent>
   <router-view/>
 </template>
 
--- frontend/whiteboard-frontend/src/components/NavbarComponent.vue
+++ frontend/whiteboard-frontend/src/components/NavbarComponent.vue
@@ -1,39 +1,31 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom mb-5">
-        <div class="container justify-content-center align-items-center">
-            <a class="navbar-brand " href="#/">
-                <img src="../assets/icons/Logo.png" width="135" height="38">
+    <nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="../assets/icons/Logo.png" width="135" height="38">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#/">Home</a>
-                    </li>
-                    <li v-if="isLogged" class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#/addWhiteboard">Whiteboards</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contacts</a>
-                    </li>
-                </ul>
-            </div>
-            <div v-if="!isLogged" class="text-end">
+        </div>
+        <ul class="nav nav-pills col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li class="nav-item"><a href="#/" class="nav-link px-2">Home</a></li>
+            <li class="nav-item" v-if="isLogged"><a href="#/addwhiteboard" class="nav-link px-2">Whiteboards</a></li>
+            <li class="nav-item" v-if="isLogged"><a href="#/profile" class="nav-link px-2">Settings</a></li>
+            <li class="nav-item"><a href="#/" class="nav-link px-2">Contacts</a></li>
+        </ul>
+        <div class="col-md-3">
+            <div v-if="!isLogged">
                 <a role="button" class="btn btn-light mx-2"  href="#/login">Sign In</a>
                 <a role="button" class="btn btn-outline-success" href="#/register">Sign Up</a>
             </div>
-            <div v-else class="dropdown text-end">
-                <a role="button" class="btn btn-light d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true">
+            <div v-else class="dropdown ">
+                <a role="button" class="btn btn-light link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="true">
                     <img src="../assets/icons/person-circle.svg" alt="mdo" width="32" height="32" class="rounded-circle">
-                </a>
-                <ul class="dropdown-menu text-small" data-popper-placement="bottom-end" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate3d(0px, 34.4px, 0px);">
-                    <li><a class="dropdown-item" href="#/addWhiteboard">New project</a></li>
-                    <li><a class="dropdown-item" href="#/profile">Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item  link-danger" role="button" @click="logout">Sign out</a></li>
-                </ul>
+                    </a>
+                    <ul class="dropdown-menu text-small" data-popper-placement="bottom-end" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate3d(0px, 34.4px, 0px);">
+                        <li><a class="dropdown-item" href="#/addWhiteboard">New project</a></li>
+                        <li><a class="dropdown-item" href="#/profile">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item  link-danger" role="button" @click="logout">Sign out</a></li>
+                    </ul>
             </div>
         </div>
     </nav>
